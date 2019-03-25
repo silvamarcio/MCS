968b81b1d0f389389dc71e17007dc45dd0a6b013
silvamarcio <silva.marcio86@hotmail.com>
2019-03-25 13:33:50 -0300

    all controllers

---
+++ MCS/Controllers/BalanceSheetsController.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MCS.Models;
+
+namespace MCS.Controllers
+{
+    public class BalanceSheetsController : Controller
+    {
+        private readonly MCSContext _context;
+
+        public BalanceSheetsController(MCSContext context)
+        {
+            _context = context;
+        }
+
+        // GET: BalanceSheets
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.BalanceSheet.ToListAsync());
+        }
+
+        // GET: BalanceSheets/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var balanceSheet = await _context.BalanceSheet
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (balanceSheet == null)
+            {
+                return NotFound();
+            }
+
+            return View(balanceSheet);
+        }
+
+        // GET: BalanceSheets/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BalanceSheets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Balance")] BalanceSheet balanceSheet)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(balanceSheet);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(balanceSheet);
+        }
+
+        // GET: BalanceSheets/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var balanceSheet = await _context.BalanceSheet.FindAsync(id);
+            if (balanceSheet == null)
+            {
+                return NotFound();
+            }
+            return View(balanceSheet);
+        }
+
+        // POST: BalanceSheets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Balance")] BalanceSheet balanceSheet)
+        {
+            if (id != balanceSheet.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(balanceSheet);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BalanceSheetExists(balanceSheet.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(balanceSheet);
+        }
+
+        // GET: BalanceSheets/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var balanceSheet = await _context.BalanceSheet
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (balanceSheet == null)
+            {
+                return NotFound();
+            }
+
+            return View(balanceSheet);
+        }
+
+        // POST: BalanceSheets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var balanceSheet = await _context.BalanceSheet.FindAsync(id);
+            _context.BalanceSheet.Remove(balanceSheet);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BalanceSheetExists(int id)
+        {
+            return _context.BalanceSheet.Any(e => e.Id == id);
+        }
+    }
+}
